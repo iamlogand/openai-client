fd354859391166d2de86d9f998903514356ce995
Logan Davidson <logan.davidson@autorek.com>
2023-05-05 13:58:01 +0100

    Use cookies for credentials

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,27 +3,27 @@ import Image from 'next/image';
 import { AxiosResponse } from "axios";
 import { ChatCompletionRequestMessage, Configuration, CreateChatCompletionResponse, OpenAIApi } from "openai";
 import styles from './index.module.css';
+import useCookies from "@/hooks/useCookies";
 
 const Home = () => {
   const [inputText, setInputText] = useState<string>('');
   const [conversation, setConversation] = useState<ChatCompletionRequestMessage[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
-  const [secretKey, setSecretKey] = useState<string>("");
-  const [orgId, setOrgId] = useState<string>("");
-  const [dialogOpen, setDialogOpen] = useState<boolean>(false);
+  const [secretKey, setSecretKey] = useCookies<string>("secretKey", "");
+  const [orgId, setOrgId] = useCookies<string>("orgId", "");
+  const [settingsOpen, setSettingsOpen] = useState<boolean>(false);
 
-  const setCredentials = (event: SyntheticEvent) => {
+  const closeSettings = (event: SyntheticEvent) => {
     event.preventDefault();
-    setDialogOpen(false);
+    setSettingsOpen(false);
   }
 
   const handleInputChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setInputText(event.target.value);
   }
 
-  const handleSubmit = async (event: SyntheticEvent) => {
+  const handleAddMessageSubmit = async (event: SyntheticEvent) => {
     event.preventDefault();
-    setLoading(true);
 
     if (inputText !== '') {
       
@@ -31,7 +31,17 @@ const Home = () => {
         role: "user",
         content: inputText
       }
-      const updatedConversation = [...conversation, newMessage];
+      setConversation([...conversation, newMessage]);
+      setInputText('');
+    }
+  }
+
+  const handleRequestMessage = async (event: SyntheticEvent) => {
+    event.preventDefault();
+    setLoading(true);
+
+    if (conversation.length > 0) {
+      
       
       const configuration = new Configuration({
         organization: orgId,
@@ -44,7 +54,7 @@ const Home = () => {
       try {
         response = (await openai.createChatCompletion({
           model: "gpt-3.5-turbo",
-          messages: updatedConversation,
+          messages: conversation,
         })) as AxiosResponse<CreateChatCompletionResponse>;
       } catch (error) {
         console.log(error);
@@ -53,13 +63,12 @@ const Home = () => {
       const choices = response?.data?.choices;
 
       if (choices && choices.length > 0 && choices[0].message?.content){
-
         const newMessage: ChatCompletionRequestMessage = {
           role: "assistant",
           content: choices[0].message?.content
         }
-        setConversation([...updatedConversation, newMessage]);
-        setInputText('');
+
+        setConversation([...conversation, newMessage]);
       }
       else {
         console.log('No response from OpenAI');
@@ -72,57 +81,68 @@ const Home = () => {
   return (
     <main className={styles.pageContent}>
       
-      <dialog open={dialogOpen} className={styles.dialog}>
-        <form onSubmit={setCredentials}>
-          <label>Secret Key</label>
-          <input type="text" value={secretKey} onChange={(event) => setSecretKey(event.target.value)}></input>
-          <label>Organization ID</label>
-          <input type="text" value={orgId} onChange={(event) => setOrgId(event.target.value)}></input>
-          <button type="submit">Save and close</button>
-        </form>
-      </dialog>
 
       <div className={styles.header}>
         <h1>OpenAI Client</h1>
-        <button onClick={() => setDialogOpen(true)}>Set credentials</button>
+        <button onClick={() => setSettingsOpen(true)} style={{width: "80px"}}>Settings</button>
       </div>
+      
+      { settingsOpen &&
+        <div className={styles.conversationWrapper}>
+          <div className={styles.settings}>
+            <h2>Settings</h2>
+            <p>You can generate a secret key by signing up for an account at <a href="https://platform.openai.com/" target="_blank">OpenAI</a> and adding a payment method. These values are only stored in your local cookies.</p>
+            <form onSubmit={closeSettings}>
+              <label>Secret Key</label>
+              <input type="text" value={secretKey} onChange={(event) => setSecretKey(event.target.value)}></input>
+              <label>Organization ID</label>
+              <input type="text" value={orgId} onChange={(event) => setOrgId(event.target.value)}></input>
+              <button type="submit">Close</button>
+            </form>
+          </div>
+        </div>
+      }
 
-      <div className={styles.conversationWrapper}>
-        <div className={styles.conversation}>
-          {
-            conversation.map((message, index) => (
-              <div key={index} className={`${styles.message} ${message.role === 'user' ? styles.user : styles.assistant}`}>
-                <div className={styles.role}>{message.role === 'user' ? 'You' : 'OpenAI'}</div>
-                <div>{message.content}</div>
-              </div>
-            ))
-          }
-        </div>
-        <div className={styles.formWrapper}>
-          <form onSubmit={handleSubmit}>
-            <textarea
-              name="name"
-              value={inputText}
-              onChange={handleInputChange}
-            />
-            {!dialogOpen ?
-              <>
-                {loading ?
-                  <div className={`${styles.submitButton} button`}>
-                    <Image src={require('../images/throbber.gif')} alt="loading" />
-                  </div>
-                  :
-                  <button type="submit" className={styles.submitButton}>Send</button>
-                }
-              </>
-              :
-              <>
-                <div className={`${styles.submitButton} button`}>...</div>
-              </>
+      { !settingsOpen &&
+        <div className={styles.conversationWrapper}>
+          <div className={styles.conversation}>
+            {
+              conversation.map((message, index) => (
+                <div key={index} className={`${styles.message} ${message.role === 'user' ? styles.user : styles.assistant}`}>
+                  <div className={styles.role}>{message.role === 'user' ? 'You' : 'OpenAI'}</div>
+                  <div>{message.content}</div>
+                </div>
+              ))
             }
-          </form>
+          </div>
+          <div className={styles.formWrapper}>
+            <form onSubmit={handleAddMessageSubmit}>
+              <textarea
+                name="name"
+                value={inputText}
+                onChange={handleInputChange}
+              />
+                
+                
+                <div className={styles.buttonArea}>
+                  {loading ?
+                    <>
+                      <button className={`${styles.submitButton} ${styles.disabled}`} disabled>Add</button>
+                      <div className={`${styles.submitButton} ${styles.request} button`}>
+                        <Image src={require('../images/throbber.gif')} alt="loading" />
+                      </div>
+                    </>
+                    :
+                    <>
+                      <button type="submit" className={styles.submitButton}>Add</button>
+                      <button className={`${styles.submitButton} ${styles.request}`} onClick={handleRequestMessage}>Request</button>
+                    </>
+                  }
+                </div>
+            </form>
+          </div>
         </div>
-      </div>
+      }
       
     </main>
   )
